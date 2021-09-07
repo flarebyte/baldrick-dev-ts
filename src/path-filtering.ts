f7d5cdcf0134702d00585080f9904bd78ff4ce7e
Olivier Huin <olih@users.noreply.github.com>
2021-09-07 09:29:55 +0100

    formatting

--- src/path-filtering.ts
+++ src/path-filtering.ts
@@ -4,12 +4,15 @@ export const byFileQuery =
   (query: FileFiltering) =>
   (pathInfo: PathInfo): boolean => {
     return (
-      query.withPathStarting.some(pathInfo.path.startsWith) ||
-      query.withExtension.some(pathInfo.path.endsWith) ||
-      query.withPathSegment.some(pathInfo.path.includes)
-      || query.withTag.some(pathInfo.tags.includes)
-    ) && !(query.withoutPathStarting.some(pathInfo.path.startsWith) ||
-    query.withoutExtension.some(pathInfo.path.endsWith) ||
-    query.withoutPathSegment.some(pathInfo.path.includes)
-    || query.withoutTag.some(pathInfo.tags.includes));
+      (query.withPathStarting.some(pathInfo.path.startsWith) ||
+        query.withExtension.some(pathInfo.path.endsWith) ||
+        query.withPathSegment.some(pathInfo.path.includes) ||
+        query.withTag.some(pathInfo.tags.includes)) &&
+      !(
+        query.withoutPathStarting.some(pathInfo.path.startsWith) ||
+        query.withoutExtension.some(pathInfo.path.endsWith) ||
+        query.withoutPathSegment.some(pathInfo.path.includes) ||
+        query.withoutTag.some(pathInfo.tags.includes)
+      )
+    );
   };
