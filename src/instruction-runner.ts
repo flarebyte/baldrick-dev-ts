e9f24caf84b92260ab3af4939ac356c4d34d7fc3
Olivier Huin <olih@users.noreply.github.com>
2021-10-03 19:59:33 +0200

    Adds instruction-runner

---
+++ src/instruction-runner.ts
@@ -0,0 +1,63 @@
+import { MicroInstruction, PathInfo } from './model';
+
+const runFilesInstruction = async (
+  _instruction: MicroInstruction
+): Promise<PathInfo[]> => {
+  return await Promise.resolve([]);
+};
+
+const runLoadInstruction = async (
+  _instruction: MicroInstruction
+): Promise<PathInfo[]> => {
+  return await Promise.resolve([]);
+};
+
+const runGlobInstruction = async (
+  _instruction: MicroInstruction
+): Promise<PathInfo[]> => {
+  return await Promise.resolve([]);
+};
+
+const runFilterInstruction = (
+  _instruction: MicroInstruction,
+  pathInfos: PathInfo[]
+): PathInfo[] => {
+  return pathInfos;
+};
+
+const runLintInstruction = async (
+  _instruction: MicroInstruction,
+  _pathInfos: PathInfo[]
+): Promise<string> => {
+  return await Promise.resolve('123');
+};
+
+const runInstructions = async (
+  instructions: MicroInstruction[]
+): Promise<void> => {
+  const filesInstruction = instructions.find((instr) => instr.name === 'files');
+  const loadInstruction = instructions.find((instr) => instr.name === 'load');
+  const globInstruction = instructions.find((instr) => instr.name === 'glob');
+  const filterInstruction = instructions.find(
+    (instr) => instr.name === 'filter'
+  );
+  const lintInstruction = instructions.find((instr) => instr.name === 'lint');
+
+  const files = filesInstruction
+    ? await runFilesInstruction(filesInstruction)
+    : [];
+  const loaded = loadInstruction
+    ? await runLoadInstruction(loadInstruction)
+    : [];
+  const globed = globInstruction
+    ? await runGlobInstruction(globInstruction)
+    : [];
+
+  const allFileInfos = [...files, ...loaded, ...globed];
+  const filtered = filterInstruction
+    ? runFilterInstruction(filterInstruction, allFileInfos)
+    : allFileInfos;
+  const linted = lintInstruction
+    ? await runLintInstruction(lintInstruction, filtered)
+    : false;
+};
