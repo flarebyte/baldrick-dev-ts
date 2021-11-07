82b70d8dc7dabbe21db272ddf586271de8a21a79
Olivier Huin <olih@users.noreply.github.com>
2021-11-07 14:33:58 +0000

    whats up

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,46 @@
 #!/usr/bin/env node
 
-import { Commanding } from './commanding';
+// import { Commanding } from './commanding';
 import { cmdLintAction } from './commanding-action';
+import { LintActionOpts, RunnerContext } from './model';
+import { basicFormatter } from './term-formatter';
 
-const commanding = new Commanding();
-commanding.declareLintAction(cmdLintAction);
+const ctx: RunnerContext = {
+  currentPath: process.cwd(),
+  termFormatter: basicFormatter,
+};
 
-async function main() {
-  await commanding.parseAsync(process.argv);
-}
-try {
-  await main();
-} catch (err) {
-  console.error(err);
-}
+const lintOpts: LintActionOpts = {
+  flags: [`lint:check`, `ecma:2019`],
+  fileSearching: {
+    pathInfos: [],
+    filtering: {
+      withPathStarting: ['src'],
+      withoutPathStarting: [],
+      withExtension: [],
+      withoutExtension: [],
+      withPathSegment: [],
+      withoutPathSegment: [],
+      withTag: [],
+      withoutTag: [],
+      withTagStarting: [],
+      withoutTagStarting: [],
+    },
+  },
+  ecmaVersion: 2019,
+  report: [],
+};
+
+await cmdLintAction(ctx, lintOpts);
+
+// const commanding = new Commanding();
+// commanding.declareLintAction(cmdLintAction);
+
+// async function main() {
+//   await commanding.parseAsync(process.argv);
+// }
+// try {
+//   await main();
+// } catch (err) {
+//   console.error(err);
+// }
