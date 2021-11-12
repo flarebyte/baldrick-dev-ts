0a4c16a2bccf3950a40c333f6f3b414671afcd8f
Olivier Huin <olih@users.noreply.github.com>
2021-11-12 09:30:37 +0000

    removes empty fields in console

--- src/term-formatter.ts
+++ src/term-formatter.ts
@@ -1,13 +1,23 @@
 import { TermFormatterFormat, TermFormatterParams } from './model';
 
-const simplifyJson = (value: string): string => value.replace(/["']/g, ' ')
+const simplifyObj = (obj: object): object => {
+  const values = Object.entries(obj);
+  const relevantValues = values.filter(
+    (keyObj) => !['[]', '', 'null'].includes(`${keyObj[1]}`)
+  );
+  return Object.fromEntries(relevantValues);
+};
+
+const simplifyJson = (value: string): string => value.replace(/["']/g, ' ');
 
 const toJsonish = (format: TermFormatterFormat, value: object): string =>
-format === 'human' ? simplifyJson(JSON.stringify(value)): JSON.stringify(value);
+  format === 'human'
+    ? simplifyJson(JSON.stringify(simplifyObj(value)))
+    : JSON.stringify(value);
 
 export const basicFormatter = (params: TermFormatterParams) => {
   const detail =
-     typeof params.detail === 'string'
+    typeof params.detail === 'string'
       ? params.detail
       : toJsonish(params.format, params.detail);
 
