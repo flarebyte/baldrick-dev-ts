539e0033b32e99e5aff97faedce137c855a6acb0
Olivier Huin <olih@users.noreply.github.com>
2021-11-20 18:32:18 +0000

    refactor to make it easier to implement additional tests

--- test/commanding-lint.test.ts
+++ test/commanding-lint.test.ts
@@ -1,20 +1,24 @@
 import { Commanding } from '../src/commanding';
 
+const actAndGetSecondParam = async (given: string[]) => {
+  const commanding = new Commanding();
+  const mockedAction = jest.fn();
+  commanding.declareLintAction(mockedAction);
+  await commanding.parseAsync(['node', 'baldrick', 'lint', ...given]);
+  const secondParam = mockedAction.mock.calls[0][1];
+  return secondParam;
+};
+
 describe('Commands Lint', () => {
   it('check each json with schemas', async () => {
-    const given = [
+    const secondParam = await actAndGetSecondParam([
       'check',
       '--with-path-starting',
       'src',
       'test',
       '--ecma-version',
       '2019',
-    ];
-    const commanding = new Commanding();
-    const mockedAction = jest.fn();
-    commanding.declareLintAction(mockedAction);
-    await commanding.parseAsync(['node', 'baldrick', 'lint', ...given]);
-    const secondParam = mockedAction.mock.calls[0][1]
+    ]);
     expect(secondParam).toMatchInlineSnapshot(`
       Object {
         "ecmaVersion": 2019,
