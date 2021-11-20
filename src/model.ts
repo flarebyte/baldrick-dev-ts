a0cee9166b92823cf46e2081d548b8a128d986f7
Olivier Huin <olih@users.noreply.github.com>
2021-11-20 14:46:27 +0000

    deletes unused code

--- src/model.ts
+++ src/model.ts
@@ -1,93 +1,7 @@
 import { ESLint } from 'eslint';
 
-export type ObjectWithKeys = { [key: string]: any };
-
-export interface PackageJson {
-  name: string;
-  source?: string;
-  jest?: ObjectWithKeys;
-  eslint?: ObjectWithKeys;
-  prettier?: ObjectWithKeys;
-  dependencies?: { [packageName: string]: string };
-  devDependencies?: { [packageName: string]: string };
-  engines?: {
-    node?: string;
-  };
-}
-interface SharedOpts {
-  target: 'node' | 'browser';
-  // Path to tsconfig file
-  tsconfig?: string;
-  // Is error extraction running?
-  extractErrors?: boolean;
-}
-export type ModuleFormat = 'cjs' | 'umd' | 'esm' | 'system';
-
-export interface BuildOpts extends SharedOpts {
-  name?: string;
-  entry?: string | string[];
-  format: 'cjs,esm';
-  target: 'browser';
-}
-
-export interface WatchOpts  {
-  target: 'node' | 'browser';
-  // Path to tsconfig file
-  tsconfig?: string;
-  // Is error extraction running?
-  extractErrors?: boolean;
-  name?: string;
-  entry?: string | string[];
-  format: 'cjs,esm';
-  verbose?: boolean;
-  noClean?: boolean;
-  onFirstSuccess?: string;
-  onSuccess?: string;
-  onFailure?: string;
-}
-
-export interface NormalizedOpts
-  extends Omit<WatchOpts, 'name' | 'input' | 'format'> {
-  name: string;
-  input: string[];
-  format: [ModuleFormat, ...ModuleFormat[]];
-}
-
-export interface JestOpts {
-  config?: string;
-}
-
-export interface LintOpts {
-  fix: boolean;
-  writeFile: boolean;
-  reportFile: string;
-  maxWarnings: number;
-}
-
-export interface CoreRollupOptions {
-  target: 'node' | 'browser';
-  // Path to tsconfig file
-  tsconfig?: string;
-  // Is error extraction running?
-  extractErrors?: boolean;
-
-  // Name of package
-  name: string;
-  // path to file
-  input: string;
-  // Environment
-  env: 'development' | 'production';
-  // Module format
-  format: ModuleFormat;
-  // Is minifying?
-  minify?: boolean;
-  // Is this the very first rollup config (and thus should one-off metadata be extracted)?
-  writeMeta?: boolean;
-  // Only transpile, do not type check (makes compilation faster)
-  transpileOnly?: boolean;
-}
-
-type PresetRollupOptionsStrategy = 'development' | 'production'
+type PresetRollupOptionsStrategy = 'development' | 'production';
+type ModuleFormat = 'cjs' | 'umd' | 'esm' | 'system';
 
 export interface PresetRollupOptions {
   buildFolder: string;
@@ -95,32 +9,6 @@ export interface PresetRollupOptions {
   input: string;
   strategy: PresetRollupOptionsStrategy;
   format: ModuleFormat;
-}
-
-export type ProfileName = 'ts-lib' | 'ts-cli';
-
-export interface Coverage {
-  ignore: string[];
-  branches: number;
-  functions: number;
-  lines: number;
-  statements: number;
-}
-
-export interface Linting {
-  ignore: string[];
-}
-
-export interface ToolOptions {
-  profileName: ProfileName;
-  sizeLimitKB: number;
-  coverage: Coverage;
-  linting: Linting;
-}
-
-export interface LocalSetup {
-  modulePath: string;
-  toolOptions: ToolOptions;
 }
 
 export interface PathInfo {
@@ -145,8 +33,6 @@ export interface FileSearching {
   pathInfos: PathInfo[];
   filtering: FileFiltering;
 }
-
-export type FilterArgs = {};
 
 export interface StatusRecord {
   title: string;
--- src/package-json.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs-extra";
-import { PackageJson } from "./model";
-import { paths } from "./path-helper";
-
-
-export const readPackageJsonSync = (): PackageJson => {
-    const appPackageJson: PackageJson = fs.readJSONSync(paths.appPackageJson);
-    return appPackageJson;
-}
