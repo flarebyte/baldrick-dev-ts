a63b33cb6b3827c5e44ef3db8f02790c2b207e58
Olivier Huin <olih@users.noreply.github.com>
2021-11-20 18:25:49 +0000

    uses mock and snapshot

--- test/commanding-lint.test.ts
+++ test/commanding-lint.test.ts
@@ -1,39 +1,47 @@
 import { Commanding } from '../src/commanding';
-import { LintAction, LintActionOpts, RunnerContext } from '../src/model';
-
-interface Example {
-  given: string[];
-}
-
-const examples: Example[] = [
-  {
-    given: ['--with-path-starting', 'src', 'test', '--ecma-version', '2019'],
-  },
-];
-const mockLintAction: LintAction = async (
-  _ctx: RunnerContext,
-  _opts: LintActionOpts
-) => {
-  await Promise.resolve('does not matter');
-};
 
 describe('Commands Lint', () => {
-  const commanding = new Commanding();
-  commanding.declareLintAction(mockLintAction);
-
-  it.each(examples)('check each json with schemas $given', async ({ given }) => {
+  it('check each json with schemas', async () => {
+    const given = [
+      'check',
+      '--with-path-starting',
+      'src',
+      'test',
+      '--ecma-version',
+      '2019',
+    ];
+    const commanding = new Commanding();
+    const mockedAction = jest.fn();
+    commanding.declareLintAction(mockedAction);
     await commanding.parseAsync(['node', 'baldrick', 'lint', ...given]);
-    const { params } = commanding.getInstrumentation().getLastRecord();
-    expect(params).toHaveLength(2);
-    expect(params[0]).toContain('/baldrick-dev-ts');
-    const latest = JSON.parse(params[1]);
-    expect(latest).toMatchObject({
-      fileSearching: {
-        filtering: {
-          withPathStarting: ['src', 'test'],
+    const secondParam = mockedAction.mock.calls[0][1]
+    expect(secondParam).toMatchInlineSnapshot(`
+      Object {
+        "ecmaVersion": 2019,
+        "fileSearching": Object {
+          "filtering": Object {
+            "withExtension": Array [],
+            "withPathSegment": Array [],
+            "withPathStarting": Array [
+              "src",
+              "test",
+            ],
+            "withTag": Array [],
+            "withTagStarting": Array [],
+            "withoutExtension": Array [],
+            "withoutPathSegment": Array [],
+            "withoutPathStarting": Array [],
+            "withoutTag": Array [],
+            "withoutTagStarting": Array [],
+          },
+          "pathInfos": Array [],
         },
-      },
-      ecmaVersion: 2019,
-    });
+        "flags": Array [
+          "lint:check",
+          "ecma:2019",
+        ],
+        "reportBase": "report/lint-report",
+      }
+    `);
   });
 });
